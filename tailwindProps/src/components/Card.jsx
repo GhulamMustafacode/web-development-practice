3fccc2eb959de4478081f4cc525ec8d9354e71e4
unknown <ghulammustafa.code@gmail.com>
2024-12-26 01:26:30 +0500

    tailwind & props project

---
+++ tailwindProps/src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Card({username,job="no Job"}) {
+    return (
+        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img className="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512" />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption>
+            <div>
+              {username}
+            </div>
+            <div>
+              {job}
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    )
+}
+
+export default Card
